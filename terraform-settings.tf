c39b9c060c68e3926b2ed85f6313e765549e3ed8
Jeevan Shrestha <jeevan.shrestha09@gmail.com>
2025-05-25 01:21:38 +1000

    Initialize terraform

---
+++ terraform-settings.tf
@@ -0,0 +1,20 @@
+terraform {
+
+    required_version = ">= 1.0.0"
+
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+        }
+        random = {
+        source  = "hashicorp/random"
+        version = "~> 3.0"
+        }
+    }
+    
+    provider_meta  "aws"{
+      region = "ap-southeast-2"
+      profile = "default"
+    }
+}
